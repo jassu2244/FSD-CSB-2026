4b55e52f24a45a4d8280f15a1a03107e3cef0ade
jassu2244 <jasmeetsinghchhabra55@gmail.com>
2026-02-25 10:25:07 +0530

    created components and made setup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,15 @@
-import { useState } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './Component/Main';
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loader, setLoader] = useState(false);
-
-  async function getData() {
-    try {
-      setLoader(true);
-      // alert("Hiiiii......")
-      const serverData = await fetch('http://localhost:4004/data');
-      const jsonData = await serverData.json();
-      //console.log()
-      setData(jsonData.msg.products);
-    } catch (e) {
-      console.log(e);
-    } finally {
-      setLoader(false);
-    }
-  }
-
-  function cartItem(ele) {
-    alert(ele);
-  }
-
   return (
     <>
-      <h2>Fetching Data from Node Server</h2>
-
-      {data.map((ele) => (
-        <div style={{ border: '2px solid red' }}>
-          <h2>
-            {' '}
-            {ele.id}:{ele.title}
-          </h2>
-          <h3>{ele.description}</h3>
-          <h2>{ele.price}</h2>
-          <h2>{ele.rating}</h2>
-          <button onClick={() => cartItem(ele)}>Add to Cart</button>
-        </div>
-      ))}
-      <button onClick={getData}>FetchData</button>
-      {loader ? (
-        <h2 style={{ color: 'brown', backgroundColor: 'white' }}>
-          Data is fetching...
-        </h2>
-      ) : (
-        ''
-      )}
-      {/* {JSON.stringify(data)} */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ frontend/src/Component/Registeration.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Registeration() {
+  return <div>Registeration</div>;
+}
+
+export default Registeration;
